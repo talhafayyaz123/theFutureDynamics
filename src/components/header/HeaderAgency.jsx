1469d56deac529d2cb77705c22c55fa65c3f998d
Talha Fayyaz <42764771+talhafayyaz123@users.noreply.github.com>
2023-09-18 23:16:18 +0500

    changes

--- src/components/header/HeaderAgency.jsx
+++ src/components/header/HeaderAgency.jsx
@@ -116,11 +116,7 @@ const HeaderAgency = () => {
       >
         <div className="ptf-offcanvas-menu__header">
           <div className="ptf-language-switcher">
-            <a className="is-active" href="#">
-              Eng
-            </a>
-            <a href="#">Fra</a>
-            <a href="#">Ger</a>
+           
           </div>
           <span
             className="ptf-offcanvas-menu-icon js-offcanvas-menu-toggle"
